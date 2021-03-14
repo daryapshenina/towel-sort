c248ddd44309a7d9674a0cc0b6f3ab869ca1837e
psheninada <psheninada@elocont.ru>
2021-03-14 15:29:00 +0300

    towel

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if((!matrix)){
+    return [];
+  }
+  let newMatrix = [];
+    matrix.forEach((item,index)=>{
+      if ((index%2)==0){
+        newMatrix.push(item);
+    }
+    else newMatrix.push(item.reverse());
+
+    });
+
+  // console.log(matrix.flat(Infinity).sort());
+   return newMatrix.flat(Infinity);
 }
